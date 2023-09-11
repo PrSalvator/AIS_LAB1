ee53da61ae00682a97af874f1111c6ce54c77f46
PrSalavator <kirill.leono2016yandex.ru@gmail.com>
2023-09-11 11:41:37 +0700

    Изменил класс Car

--- Car.cs
+++ Car.cs
@@ -8,13 +8,72 @@ namespace AIS_LAB1
 {
     class Car
     {
-        public int number_of_doors { get; set; }
-        public int amount_of_horsepower { get; set; }
-        public string body_type { get; set; }
-        public string car_type { get; set; }
-        public string car_brand { get; set; }
-        public string car_model { get; set; }
-        public bool is_electric_car { get; set; }
+        public Car(int NOD, int AOH, string CT, string CB, string CM, bool IEC)
+        {
+            Number_of_doors = NOD;
+            Amount_of_horsepower = AOH;
+            Car_type = CT;
+            Car_brand = CB;
+            Car_model = CM;
+            Is_electric_car = IEC;
+        }
+        public Car() { }
+
+        private int number_of_doors;
+
+        private int amount_of_horsepower;
+
+        public int Number_of_doors
+        {
+            get
+            {
+                return number_of_doors;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    number_of_doors = 0;
+                }
+                else if(value > 5)
+                {
+                    number_of_doors = 5;
+                }
+                else
+                {
+                    number_of_doors = value;
+                }
+            }
+        }
+        public int Amount_of_horsepower
+        {
+            get
+            {
+                return amount_of_horsepower;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    amount_of_horsepower = 0;
+                }
+                else if (value > 2028)
+                {
+                    amount_of_horsepower = 2028;
+                }
+                else
+                {
+                    amount_of_horsepower = value;
+                }
+            }
+        }
+        public string Car_type { get; set; }
+
+        public string Car_brand { get; set; }
+
+        public string Car_model { get; set; }
+
+        public bool Is_electric_car { get; set; }
 
     }
 }
