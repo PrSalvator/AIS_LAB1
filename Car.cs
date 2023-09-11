71c6bd6a481b0dbd686a4197fde22471a6e5d7c1
PrSalavator <kirill.leono2016yandex.ru@gmail.com>
2023-09-11 11:00:15 +0700

    Добавил класс

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_LAB1
+{
+    class Car
+    {
+        public int number_of_doors { get; set; }
+        public int amount_of_horsepower { get; set; }
+        public string body_type { get; set; }
+        public string car_type { get; set; }
+        public string car_brand { get; set; }
+        public string car_model { get; set; }
+        public bool is_electric_car { get; set; }
+
+    }
+}
